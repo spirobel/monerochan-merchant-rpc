e1f1150b1b8f5ef7af84e81f963f1c8369e21568
spirobel <spirobel@protonmail.com>
2022-08-21 15:57:51 +0800

    ping pong example route

---
+++ app.ts
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+const OpenApiValidator = require('express-openapi-validator');
+
+const port = 3000;
+const app = express();
+const apiSpec = path.join(__dirname, 'api.yaml');
+
+// 1. Install bodyParsers for the request types your API will support
+app.use(express.urlencoded({ extended: false }));
+app.use(express.text());
+app.use(express.json());
+
+app.use('/spec', express.static(apiSpec));
+
+// 2. Add the OpenApiValidator middleware
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec,
+    validateResponses: true, // default false
+    // 3. Provide the path to the controllers directory
+    operationHandlers: path.join(__dirname), // default false
+  }),
+);
+
+// 4. Add an error handler
+app.use((err:any, req:any, res:any, next:any) => {
+  // format errors
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+http.createServer(app).listen(port);
+console.log(`Listening on port ${port}`);
+
+module.exports = app;
---
+++ routes/ping.ts
@@ -0,0 +1,3 @@
+module.exports = {
+    ping: (req:any, res:any) => res.status(200).send('pong'),
+  };
