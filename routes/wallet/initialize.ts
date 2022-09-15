425f9bc730ee8f868e35a6be169b734a045fe0fc
spirobel <spirobel@protonmail.com>
2022-09-16 03:21:31 +0800

    finalize initialize wallet api call

--- routes/wallet/initialize.ts
+++ routes/wallet/initialize.ts
@@ -1,13 +1,20 @@
 module.exports = {
   initialize: async (req:any, res:any) =>  {
-      console.log(req.body)
+
       const monerojs = require("monero-javascript");
       try{
         let wallet = await monerojs.createWalletFull(
-          req.body
+          {
+            path: req.body.path,
+            networkType: req.body.networkType,
+            primaryAddress: req.body.primaryAddress,
+            privateViewKey: req.body.privateViewKey,
+            restoreHeight: req.body.restoreHeight,
+            password: "password_is_snakeoil_in_this_case",
+
+          }
         );
-        console.log(wallet)
-        res.status(200).send('wallet initialize'+  req.body)
+        res.status(200).json({message: 'wallet files successfully created'})
       } catch (error){
         res.status(500).json({ message:'unexpected error: ' + error });
       }
