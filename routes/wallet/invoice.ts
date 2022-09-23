25bb8ffcfee525e1ae43d15aa153baacfe553afb
spirobel <spirobel@protonmail.com>
2022-09-24 02:46:57 +0800

    dont make address if no payment_id

--- routes/wallet/invoice.ts
+++ routes/wallet/invoice.ts
@@ -5,8 +5,12 @@ module.exports = {
       const monerojs = require("monero-javascript");
       let wallet = req.app.locals.wallets[req.body.path]
       try{
-        let address = await wallet.getIntegratedAddress("", req.body.payment_id.toString()) 
-        let address_qrcode = await QRCode.toDataURL(address)
+        let address = ""
+        let address_qrcode = ""
+        if(req.body.payment_id){
+          address = await wallet.getIntegratedAddress("", req.body.payment_id.toString()) 
+          address_qrcode = await QRCode.toDataURL(address)
+        } 
         let amount = req.app.locals.exchange_rate[req.body.currency] * req.body.amount
         res.status(200).json({address, address_qrcode,amount})
       } catch (error){
