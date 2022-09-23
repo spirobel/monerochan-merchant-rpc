06221eef62d0f7918eaee763a1b1a2118cff0a9e
spirobel <spirobel@protonmail.com>
2022-09-24 04:37:41 +0800

    remove wallet object member after wallet closed.

--- routes/wallet/shutdown.ts
+++ routes/wallet/shutdown.ts
@@ -1,6 +1,7 @@
-const close_wallet = async (w:any) => {
+const close_wallet = async (w:any, wallet_name:string, req:any) => {
   await w.stopSyncing()
   await w.close(true)
+  delete req.app.locals.wallets[wallet_name]
 }
 
 module.exports = {
@@ -12,8 +13,8 @@ module.exports = {
         let wallet_names = req.body
         wallet_names.forEach((wallet_name: string) => {
           let w =wallets[wallet_name];
-
-          close_wallet(w)
+          if(!w) throw wallet_name + " was not opened, so it cant be closed"
+          close_wallet(w, wallet_name, req)
     
         });
         res.status(200).json({message: 'wallet files successfully closed'})
