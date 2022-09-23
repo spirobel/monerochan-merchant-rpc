3d42e78f4387e568169c2fc7300495c7b568da4d
spirobel <spirobel@protonmail.com>
2022-09-24 04:29:16 +0800

    fix shutdown route

--- routes/wallet/shutdown.ts
+++ routes/wallet/shutdown.ts
@@ -1,9 +1,21 @@
+const close_wallet = async (w:any) => {
+  await w.stopSyncing()
+  await w.close(true)
+}
+
 module.exports = {
   shutdown: async (req:any, res:any) =>  {
       try{
-        let wallet = req.app.locals.wallets[req.body.path]
-        wallet.stopSyncing()
-        wallet.close(true)
+  
+        let wallets = req.app.locals.wallets
+        if(!wallets) throw "no wallet opened"
+        let wallet_names = req.body
+        wallet_names.forEach((wallet_name: string) => {
+          let w =wallets[wallet_name];
+
+          close_wallet(w)
+    
+        });
         res.status(200).json({message: 'wallet files successfully closed'})
       } catch (error){
         res.status(500).json({ message:'unexpected error: ' + error });
