8ed111c931ca6b36235de09374ceb70b00cc291f
spirobel <spirobel@protonmail.com>
2022-09-24 02:43:52 +0800

    first shot invoice.ts

--- routes/wallet/invoice.ts
+++ routes/wallet/invoice.ts
@@ -1,20 +1,14 @@
 module.exports = {
   invoice: async (req:any, res:any) =>  {
+    const QRCode = require('qrcode')
 
       const monerojs = require("monero-javascript");
+      let wallet = req.app.locals.wallets[req.body.path]
       try{
-        let wallet = await monerojs.createWalletFull(
-          {
-            path: req.body.path,
-            networkType: req.body.networkType,
-            primaryAddress: req.body.primaryAddress,
-            privateViewKey: req.body.privateViewKey,
-            restoreHeight: req.body.restoreHeight,
-            password: "password_is_snakeoil_in_this_case",
-
-          }
-        );
-        res.status(200).json({message: 'wallet files successfully created'})
+        let address = await wallet.getIntegratedAddress("", req.body.payment_id.toString()) 
+        let address_qrcode = await QRCode.toDataURL(address)
+        let amount = req.app.locals.exchange_rate[req.body.currency] * req.body.amount
+        res.status(200).json({address, address_qrcode,amount})
       } catch (error){
         res.status(500).json({ message:'unexpected error: ' + error });
       }
