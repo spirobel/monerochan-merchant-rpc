109336cee51ca3e8bf6f33c635bf94efc559db5e
spirobel <spirobel@protonmail.com>
2022-09-26 11:18:59 +0800

    fix sync and test callback

--- routes/wallet/sync.ts
+++ routes/wallet/sync.ts
@@ -2,26 +2,23 @@
 const monerojs = require("monero-javascript");
 
 class WalletListener extends require("monero-javascript").MoneroWalletListener {
-  constructor(wallet:any ) {
+  constructor(wallet:any, callback: string ) {
     super();
+    this.callback = callback
     this.wallet = wallet
 }
-  onNewBlock(height:any) {
-      console.log("new block", height)
-  }
+ 
   onOutputReceived(output:any) {
-      let txHash = output.getTx().getHash();
-      let isConfirmed = output.getTx().isConfirmed();
-
-
+      let tx_hash = output.getTx().getHash();
       //https://github.com/monero-ecosystem/monero-javascript/issues/60
-      this.wallet.getTx(txHash).then((tx:any) => {
+      this.wallet.getTx(tx_hash).then((tx:any) => {
         
-          let transfers = tx.getIncomingTransfers()
-          if (transfers) {
-
-              console.log("incoming transfer",this.wallet_name, txHash, transfers[0].state)
-          }
+          let payment_id = tx.getPaymentId()
+          let amount = Object.assign(new monerojs.BigInteger(), tx.getIncomingAmount()).toString()
+          let height = tx.getHeight()
+          let confirmations = tx.getNumConfirmations()
+          let isConfirmed = tx.isConfirmed()
+          axios.post(this.callback, { payment_id, amount, tx_hash, height, confirmations, isConfirmed })          
       });
   }
 }
@@ -45,7 +42,7 @@ module.exports = {
         );
         if(!req.app.locals.wallets){req.app.locals.wallets = {}}
         req.app.locals.wallets[req.body.path] = wallet
-        await wallet.addListener( new WalletListener(wallet))
+        await wallet.addListener( new WalletListener(wallet, req.body.callback))
         await wallet.startSyncing(5000)
         res.status(200).json({message: 'wallet successfully opened and sync started.'})
       } catch (error){
