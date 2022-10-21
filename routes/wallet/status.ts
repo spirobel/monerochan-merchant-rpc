635e313619bcb123b69ba8576ae69693a3c09656
spirobel <spirobel@protonmail.com>
2022-10-21 15:22:37 +0800

    fix status endpoint

--- routes/wallet/status.ts
+++ routes/wallet/status.ts
@@ -1,24 +1,33 @@
 module.exports = {
   status: async (req:any, res:any) =>  {
     let return_array: any[] = []
-      try{
+    
      
         let wallets = req.app.locals.wallets
+        if(!wallets || wallets.length === 0){
+            res.status(200).json([]);
+            return;
+        }
         let wallet_names = Object.keys(wallets)
         for (const wallet_name of wallet_names){
           let w =wallets[wallet_name];
-          
-          
-          return_array.push({ path: wallet_name,
-            current_sync_height: await w.getHeight(),
-            daemon_sync_height: await w.getDaemonHeight()
+
+          let response_object = {path: wallet_name} as any
+          try{
+          response_object.current_sync_height = await w.getHeight();
+          response_object.daemon_sync_height = await w.getDaemonHeight();
+          return_array.push(response_object);
+        } catch (error){
+          return_array.push({
+            path: wallet_name,
+            error_message: String(error)
           })
+        }
+          
         }
   
         res.status(200).json(return_array)
-      } catch (error){
-        res.status(500).json({ message:'unexpected error: ' + error });
-      }
+ 
 
     },
   };
